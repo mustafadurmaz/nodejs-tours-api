ecb934bd9d62d7e919fce75a470da9dafd8ab6c1
“mustafadurmaz” <mdmz_001@hotmail.com>
2021-12-31 11:46:51 +0300

    filtering code writed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,7 +14,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+
+    // Filtering
+    const queryObj = { ...req.query };
+    const excludeFiels = ['page', 'sort', 'limit', 'fields'];
+    excludeFiels.forEach((el) => delete queryObj[el]);
+
+    // Advanced filtering
+    let queryStr=JSON.stringify(queryObj)
+    queryStr=queryStr.replace(/\b(gte|gt|lte|lt)/g,match=>`$${match}`);
+    console.log(JSON.parse(queryStr));
+    const query = Tour.find(JSON.parse(queryStr));
+    
+    //Execute query
+    const tours = await query;
 
     res.status(200).json({
       status: 'succeess',
