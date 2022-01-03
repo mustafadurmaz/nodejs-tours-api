7275e3fdfe2ee15b3f422427ac3da3842d268234
“mustafadurmaz” <mdmz_001@hotmail.com>
2022-01-03 22:22:49 +0300

    unhandled rejection error added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,47 @@
-module.exports=(err, req, res, next) => {
-    // console.log(err.stack);
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-  
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+  } else {
+    // log error
+    console.error('error', err);
+    // generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
   }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+
+    sendErrorProd(error, res);
+  }
+};
--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection. Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
